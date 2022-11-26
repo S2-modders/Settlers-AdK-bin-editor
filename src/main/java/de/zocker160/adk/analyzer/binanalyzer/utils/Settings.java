ed0125ab1db5a81b000a83da00dfccf2820d6915
zocker-160 <zocker1600@posteo.net>
2022-11-26 17:55:49 +0100

    finalize settings class because why not

--- src/main/java/de/zocker160/adk/analyzer/binanalyzer/utils/Settings.java
+++ src/main/java/de/zocker160/adk/analyzer/binanalyzer/utils/Settings.java
@@ -1,6 +1,6 @@
 package de.zocker160.adk.analyzer.binanalyzer.utils;
 
-public class Settings {
+public final class Settings {
     public static final String APPLICATION_NAME = "AdK BIN Editor";
     public static final String VERSION = "v0.2";
 }
